7cb3d521830ad109cdfb916cf859556bcc27debd
Saad <saad.mayya007@gmail.com>
2023-10-16 23:53:47 +0500

    Variable and data types done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 144553
+let accountEmail = "saad.maya@gmail.com"
+var accountPassword = "12345"
+accountCity = "karachi"
+let accountState;
+// const = value will not change
+// accountId = 2 not allowed
+
+// {
+//     scope
+// }
+
+console.log(accountId)
+
+accountEmail = "hc@.com"
+accountPassword = "2121221112"
+accountCity = "hyderabad"
+
+/*
+    comments
+    prefer not to use var
+    because of issue in block scope and function scope
+*/
+
+
+
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState])
