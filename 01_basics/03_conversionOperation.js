3fcceeaff03486740c83335338edfc597aa9f78c
Saad Ahmad Khan <59502493+Maya-kali@users.noreply.github.com>
2023-11-03 07:27:51 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// we use string, number, null, undefined, boolean
+let score = false
+
+// start learning conversion
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueNumber = Number(score)
+// console.log( typeof valueNumber);
+// console.log( valueNumber);
+
+
+// "33" => 33   
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = ""
+
+let BooleanLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "saad" => true
+
+
+let someNumber = '33 abc'
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
