6153d8a599d87bf8d1182e8af11d2b1f2902ba8c
Saad Ahmad Khan <59502493+Maya-kali@users.noreply.github.com>
2023-11-04 10:34:11 +0530

    Comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+// console.log("2" > 1); // typescript will not allow to run this command
+// console.log("02" > 1); // datatype must be same
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+// === stick check
+
+console.log("===" +"2" === 2);
