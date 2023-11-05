2cad861772e68ae703860e799b6d91f2421919f7
Saad Ahmad Khan <59502493+Maya-kali@users.noreply.github.com>
2023-11-05 08:22:33 +0530

    DataType Summary

---
+++ 01_basics/05_datatype-summary.js
@@ -0,0 +1,48 @@
+// Primitive
+
+// 7 Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt,  
+// String = ""
+// Number = 234
+// Boolean = true and false 
+// Null = full empty
+// Undefined = create variable but not put value 
+// BigInt = 0.23424
+// Symbol = Create unique value 
+
+
+const score = 100;          // Result type: number
+const scoreValue = 100.8    // Result type: number    
+
+const isLoggedIn = false    // Result type: boolean
+const outSideTem = null     // Result type: object
+let userEmail;              // Result type: undefined
+
+const id = Symbol('123')    // Result type: symbol    
+const anOtherId = Symbol('123')
+
+console.log(id === anOtherId);
+
+
+const bigNumber = 654653264621654621n
+// Reference (Non Primitive) 
+// allocate reference in memory
+
+// Master in java : Object, browser event 
+
+// Array, Object, Functions
+
+const heros = ["Saad", "Ahmad", "Khan"] // Array
+
+// anything else in curly braces its call Object
+
+let myObj = {       //Object
+    name:"Saad1",
+    ege: "22",
+}
+
+// function(){}
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof id);
